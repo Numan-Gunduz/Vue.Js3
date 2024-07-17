2a42c39216c834a92e558a1231f87a515e542e90
213311012 <numangunduz00@gmail.com>
2024-07-17 10:34:23 +0300

    Change

---
+++ .vscode/12-Example2/script.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      value: 0,
+    };
+  },
+  computed: {
+    result() {
+      if (this.value < 25) {
+        return "25'den küçük değer buldunuz";
+      } else if (this.value == 25) {
+        return this.value;
+      } else {
+        return "25'den büyük bir değer buldunuz";
+      }
+    },
+  },
+  methods: {
+    addTen() {
+      this.value = this.value + 10;
+    },
+    addFivee() {
+      this.value = this.value - 5;
+    },
+  },
+});
+
+app.mount("#Example"); //Vue js in kontrol ettiği id değer aralığı*/
